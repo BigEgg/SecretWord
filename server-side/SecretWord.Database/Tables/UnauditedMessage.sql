ff10a3d05f91a6db818b31c385bda05d91b41316
BigEgg <cd_bigegg@hotmail.com>
2014-04-10 00:43:41 +0800

    [#1] Create the server-side .NET solution. Add some SQL scripts to create the table.

---
+++ server-side/SecretWord.Database/Tables/UnauditedMessage.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[UnauditedMessage]
+(
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [OpenId] NCHAR(16) NOT NULL, 
+    [Content] NVARCHAR(150) NOT NULL, 
+    [PostTime] DATETIME NOT NULL, 
+    [Rank] INT NOT NULL, 
+    [IsPrepared] BIT NOT NULL DEFAULT 0, 
+    [IsInvalid] BIT NOT NULL DEFAULT 0, 
+    [UpdateTime] DATETIME NOT NULL DEFAULT getdate(), 
+    [CreateTime] DATETIME NOT NULL DEFAULT getdate(), 
+    CONSTRAINT [AK_UnauditedMessage_OpenId] UNIQUE ([OpenId])
+)
+
+GO
+
+CREATE INDEX [IX_UnauditedMessage_Rank] ON [dbo].[UnauditedMessage] ([Rank])
+
+GO
+
+CREATE INDEX [IX_UnauditedMessage_OpenId] ON [dbo].[UnauditedMessage] ([OpenId])
