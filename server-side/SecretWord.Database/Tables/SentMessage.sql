72e1ea67666b3f394af1e34897317c53b6575d84
BigEgg <cd_bigegg@hotmail.com>
2014-04-10 00:38:53 +0800

    [#1] Create the server-side .NET solution. Add some SQL scripts to create the table.

---
+++ server-side/SecretWord.Database/Tables/SentMessage.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[SentMessage]
+(
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [OpenId] NCHAR(16) NOT NULL, 
+    [Content] NVARCHAR(150) NOT NULL, 
+    [PostTime] DATETIME NOT NULL, 
+    [SendTime] DATETIME NOT NULL DEFAULT getdate(), 
+    [Rank] INT NOT NULL, 
+    [UpdateTime] DATETIME NOT NULL DEFAULT getdate(), 
+    [CreateTime] DATETIME NOT NULL DEFAULT getdate(), 
+    CONSTRAINT [AK_SentMessage_OpenId] UNIQUE ([OpenId])
+)
+
+GO
+
+CREATE INDEX [IX_SentMessage_OpenId] ON [dbo].[SentMessage] ([OpenId])
+
+GO
+
+CREATE INDEX [IX_SentMessage_Rank] ON [dbo].[SentMessage] ([Rank])
